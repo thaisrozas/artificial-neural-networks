a1e133b15eee867de62c9ff07279feb999e4b4f5
ThaisRozas <thaisrozas@gmail.com>
2023-10-30 21:46:45 -0300

    perceptron algorithm

--- binary-classification/perceptron.py
+++ binary-classification/perceptron.py
@@ -2,10 +2,71 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def sign(u):
-    return 1 if u>=0 else -1
+    return 1 if u >= 0 else -1
 
-Data = np.loadtxt("/home/thais/Desktop/artificial-neural-networks/binary-classification/Data.csv",delimiter=',', skiprows=1)
+def perceptron_pseudocode(X, Y, learning_rate):
+    # Add a bias term to the input data
+    X = np.c_[np.ones(X.shape[0]), X]
+    
+    # Initialize weights with zeros or random small values
+    w = np.zeros(X.shape[1])
+    
+    error_exists = True
+    t = 0
+    
+    while error_exists:
+        error_exists = False
+        
+        for i in range(X.shape[0]):
+            u_t = np.dot(w, X[i])
+            y_t = sign(u_t)
+            w_new = w + learning_rate * (Y[i] - y_t) * X[i]
+            
+            if (Y[i] != y_t):
+                error_exists = True
+                
+            w = w_new
 
-plt.scatter(Data[0:1500,0], Data[0:1500,1], color='green', edgecolors='k')
-plt.scatter(Data[1500:,0], Data[1500:,1], color='blue', edgecolors='k')
+        t += 1
+
+    return w
+
+# Load data from an external CSV file
+data = np.loadtxt("binary-classification/Data.csv", delimiter=',')
+
+# Split data into features and labels
+#X = data[:, :-1]
+#Y = data[:, -1]
+
+inicio = 1000
+fim = 2000
+
+# Extrair as linhas entre 'inicio' e 'fim' e as colunas desejadas
+subset = data[inicio:fim, :]
+X = subset[:, :2]  # Duas primeiras colunas
+Y = subset[:, 2]   # Terceira coluna
+
+
+# Set a learning rate
+learning_rate = 0.01
+
+# Train the perceptron
+final_weights = perceptron_pseudocode(X, Y, learning_rate)
+
+# Create a scatter plot of the data points
+plt.scatter(X[Y == 1][:, 0], X[Y == 1][:, 1], color='green', edgecolors='k', label='Class 1')
+plt.scatter(X[Y == -1][:, 0], X[Y == -1][:, 1], color='blue', edgecolors='k', label='Class -1')
+
+# Plot the decision boundary
+x1 = np.linspace(X[:, 0].min() - 1, X[:, 0].max() + 1, 100)
+x2 = (-final_weights[0] - final_weights[1] * x1) / final_weights[2]
+plt.plot(x1, x2, color='red', linestyle='--', label='Decision Boundary')
+
+plt.xlim(X[:, 0].min() - 1, X[:, 0].max() + 1)
+plt.ylim(X[:, 1].min() - 1, X[:, 1].max() + 1)
+
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.legend()
+plt.title('Perceptron Decision Boundary')
 plt.show()
